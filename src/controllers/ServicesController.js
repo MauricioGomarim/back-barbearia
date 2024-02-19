878943965f058a901a3d7218a61e6b7afa0657a0
Mauricio Gomarim <mauricio.gomarim3@gmail.com>
2024-02-19 00:29:02 -0300

    parte de services

--- src/controllers/ServicesController.js
+++ src/controllers/ServicesController.js
@@ -17,7 +17,6 @@ class ServicesController {
         duracao,
         valor,
       });
-
     } catch {
       throw new AppError("Erro ao cadastrar serviço!", 500);
     }
@@ -27,44 +26,55 @@ class ServicesController {
 
   async update(request, response) {
     const { title, stars, duracao, valor } = request.body;
-  
+
     const servico_id = request.params.id;
-    
-    
+
     const servico = await knex("servicos").where({ id: servico_id }).first();
 
     if (!servico) {
       throw new AppError("Serviço não encontrado");
     }
 
-
-    servico.title = title
-    servico.stars = stars
-    servico.duracao = duracao
-    servico.valor = valor
+    servico.title = title;
+    servico.stars = stars;
+    servico.duracao = duracao;
+    servico.valor = valor;
 
     // Inserindo dados no banco
-    await knex("servicos").where({ id: servico_id }).update(servico)
+    await knex("servicos").where({ id: servico_id }).update(servico);
 
     return response.status(201).json();
   }
 
   async index(request, response) {
-    const services = await knex("servicos");
+    const { title, id } = request.query;
+    let services;
+
+    if (title) {
+      services = await knex("servicos")
+        .whereRaw("LOWER(title) LIKE ?", [`%${title.toLowerCase()}%`])
+        .orderBy("id");
+    } else if (title == "" || title == undefined) {
+      services = await knex("servicos").orderBy("created_at", "desc");
+    }
+
+    if(id) {
+      services = await knex("servicos").where("user_id", id).orderBy("created_at", "desc");
+    }
+
     return response.status(201).json(services);
   }
 
-  async show(request, response){
+  async show(request, response) {
     const id = request.params.id;
     const servico = await knex("servicos").where({ id }).first();
     return response.status(201).json(servico);
   }
 
-  async delete(request, response){
-
+  async delete(request, response) {
     const id = request.params.id;
 
-    await knex('servicos').where({ id }).delete();
+    await knex("servicos").where({ id }).delete();
     return response.status(201).json();
   }
 }
