1ce6744f89e50af9d53662df906d5b53d1f6f588
Mauricio Gomarim <mauricio.gomarim3@gmail.com>
2024-03-12 12:56:39 -0300

    fix

--- src/controllers/ReservasController.js
+++ src/controllers/ReservasController.js
@@ -30,11 +30,11 @@ class ReservasController {
 
     const { whatsapp } = request;
 
-    // try {
-    //   whatsapp.sendMessage("5568992581641" + "@c.us", "mensagem teste");
-    // } catch (error) {
-    //   console.log('error: ', error);
-    // }
+    try {
+      whatsapp.sendMessage("5517992118342" + "@c.us", "mensagem teste");
+    } catch (error) {
+      console.log('error: ', error);
+    }
     
 
     await knex("reservas").insert({
